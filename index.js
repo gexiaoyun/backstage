b615072927eb99b71d97b413ecf35d49787f6cb2
葛小云 <gexiaoyun@users.noreply.github.com>
2021-07-05 23:01:53 +0800

    ✨ feat(增加koa-cors): 增加koa-cors
    增加koa-cors,来让请求不跨域

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa2')
 const bodyParser = require('koa-bodyparser');
+const cors = require("koa2-cors");
 
 const Router = require('./router')
 const app = new Koa()
+app.use(cors())
 app.use(bodyParser()) // 解析request.body
 
 const middleWare = require('./middleware')
